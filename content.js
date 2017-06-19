c6bc38f5643fd53a12436c57aa968077ed965fca
Irvin Lee Eduliantes <irvin@d3fy.com>
2017-06-19 18:46:45 +0800

    fixed popup position; made sure that it doesn't overflow outside a scrollable container; added popup balloon tail

--- content.js
+++ content.js
@@ -13,17 +13,26 @@ jQuery.expr[':'].regex = function(elem, index, match) {
     return regex.test(jQuery(elem)[attr.method](attr.property));
 }
 
+var balloonTailHTML = '<img class="balloon-tail-img" src="'+chrome.extension.getURL('balloon-tail.png') + '" />';
+var thePopupHTML = '<div class="d3fy-droplr-preview-popup"></div>';
+
 var popupStyles = {
   width: '300px',
   height: '200px',
-  position: 'absolute',
+  position: 'fixed',
+  'z-index': '9999998',
   'background-color': 'white',
   border: '2px #000 solid',
   top: '-210px',
-  left: '0'
+  left: '0',
+  overflow: 'visible',
+  padding: '5px'
 };
 
-var thePopupHTML = $('<div class="d3fy-droplr-preview-popup"></div>')
+var balloonTailStyles = {
+  position: 'fixed',
+  'z-index': '9999999',
+};
 
 function fetchDroplrAsset(droplrUrl){
   return $.ajax({
@@ -31,6 +40,7 @@ function fetchDroplrAsset(droplrUrl){
       type: "GET"
   });
 }
+
 
 $('body').on('mouseenter mouseleave', 'a:regex(href, d3fy.xyz)', function(event){
   var el = $(this);
@@ -42,25 +52,62 @@ $('body').on('mouseenter mouseleave', 'a:regex(href, d3fy.xyz)', function(event)
       if(el.attr('hovered') == '1'){
         el.css({'position': 'relative'});
         var thePopup = $(thePopupHTML);
-        var existingPopup = el.find('.d3fy-droplr-preview-popup');
+        var thePreviewImg = $('<div></div>');
 
-        if(existingPopup.length){
-          existingPopup.show();
-        }else{
-          thePopup.css(popupStyles);
-          var leftPos = el.innerWidth() - 300;
-          thePopup.css({
+        thePopup.css(popupStyles);
+
+        var existingPopup = el.find('.d3fy-droplr-preview-popup');
+        var balloonTail = el.find('.balloon-tail-img');
+
+
+        if(!existingPopup.length){
+          thePreviewImg.css({
+            'width': '100%',
+            'height': '100%',
             'background-image': 'url("https://dfetch.d3fy.xyz?target_url='+el.attr('href')+'")',
-            'background-size': 'cover',
-            'left': leftPos + 'px'
+            'background-size': 'cover'
           });
+
+          thePopup.append(thePreviewImg);
+
+          el.prepend($(balloonTailHTML).css(balloonTailStyles));
           el.prepend(thePopup);
-          thePopup.show();
+
+          existingPopup = el.find('.d3fy-droplr-preview-popup');
+          balloonTail = el.find('.balloon-tail-img');
         }
+
+        var leftPos = 0;
+        var scrollableParent = el.parents('.scrollable:first');
+
+        if(scrollableParent.length){
+          var rightBoundary = scrollableParent.offset().left + scrollableParent.innerWidth();
+
+          if(el.offset().left + 300 > rightBoundary){
+            leftPos = rightBoundary - 310;
+          }else{
+            leftPos = el.offset().left;
+          }
+        }else{
+          leftPos = el.offset().left;
+        }
+
+        existingPopup.css({
+          'left': leftPos + 'px',
+          'top': el.offset().top - 212 + 'px'
+        });
+
+        balloonTail.css({
+          'left': el.offset().left + Math.floor(el.width() / 2) + 'px',
+          'top': el.offset().top + 'px'
+        });
+
+        existingPopup.show();
+        balloonTail.show();
       }
     }, 750);
   }else{
     el.attr('hovered', '0');
-    el.find('.d3fy-droplr-preview-popup').hide();
+    el.find('.d3fy-droplr-preview-popup, .balloon-tail-img').hide();
   }
 });
